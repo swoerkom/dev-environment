3a25126e8a2747d76f2c1e58ca5a86544ce77220
swoerkom <swoerkom@spartaglobal.com>
2019-05-13 16:47:59 +0100

    Adding files

---
+++ environment/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
+sudo apt-get install nodejs -y
+sudo npm install pm2 -g
+sudo systemctl start nginx
+cd /app/
+pm2 start /app/app.js
