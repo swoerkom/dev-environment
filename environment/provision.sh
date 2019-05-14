32f257b4c400ceab16cf1cfdbc13717cca647f53
swoerkom <49191108+swoerkom@users.noreply.github.com>
2019-05-14 10:07:42 +0100

    Update provision.sh

--- environment/provision.sh
+++ environment/provision.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
 sudo apt-get install nodejs -y
-sudo npm install pm2 -g
-sudo systemctl start nginx
+npm install -g
+npm install pm2 -g
 cd /app/
 pm2 start /app/app.js
